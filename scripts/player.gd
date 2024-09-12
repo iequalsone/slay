1d0749d9a3f494116d8c29e115d65ee3522c3ba9
Jon Howard <johnny.rtn@gmail.com>
2024-09-12 11:29:11 -0230

    change player sprite, update animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal hit
 
-@export var speed = 400 
+@export var speed = 200 
 var screen_size
 
 var projectile_speed = 600
@@ -45,23 +45,21 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+	update_animation(velocity, delta)
+
+func update_animation(velocity: Vector2, delta: float) -> void:
+	velocity = velocity.normalized() * speed
+	$AnimatedSprite2D.play()
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0:
+	if velocity.x != 0 || velocity.y != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation  = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
-
+	else:
+		$AnimatedSprite2D.animation = "idle"
 
 func _physics_process(delta: float) -> void:
 	var screen_size = get_viewport_rect().size
